04955a5ccb54bc166c09af5b42390d50c918037c
Sofia Hnatiuk <sofia.hnatiuk46@gmail.com>
2024-10-03 12:30:17 +0300

    static files settings

--- website/website/catalog/urls.py
+++ website/website/catalog/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
+
 from .views import ComponentListView
 
 urlpatterns = [
     path('index/', ComponentListView.as_view(), name='component-list'),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
