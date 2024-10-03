afcea2c094c0d72607464c6947ad673d61debba1
Sofia Hnatiuk <sofia.hnatiuk46@gmail.com>
2024-10-03 12:31:52 +0300

    static files settings

--- website/website/website/settings.py
+++ website/website/website/settings.py
@@ -119,6 +119,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Set the path to your static files
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
