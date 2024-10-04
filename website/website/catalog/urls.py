4de566a8f2fcc23eb250fe20f640526470f87c34
Sofia Hnatiuk <sofia.hnatiuk46@gmail.com>
2024-10-04 16:06:04 +0300

    detail view

--- website/website/catalog/urls.py
+++ website/website/catalog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import ComponentListView
+from .views import ComponentListView, ComponentDetailView
 
 urlpatterns = [
     path('', ComponentListView.as_view(), name='component-list'),
+    path('component/<int:pk>/', ComponentDetailView.as_view(), name='component-detail'),
 ]
 
--- website/website/catalog/views.py
+++ website/website/catalog/views.py
@@ -2,12 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Component
 
+class ComponentDetailView(DetailView):
+    model = Component
+    template_name = 'catalog/detail.html'
+    context_object_name = 'component'
 
 class ComponentListView(ListView):
     model = Component
-    template_name = 'catalog/index.html'  # Define your template
+    template_name = 'catalog/index.html'
     context_object_name = 'components'
     paginate_by = 10  # Number of components per page
