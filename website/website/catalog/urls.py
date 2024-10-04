503b17d24bf1b6c0d09d7879ee24f852ff24bfbb
Sofia Hnatiuk <sofia.hnatiuk46@gmail.com>
2024-10-04 21:17:26 +0300

    category view

--- website/website/catalog/urls.py
+++ website/website/catalog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import ComponentListView, ComponentDetailView
+from .views import ComponentListView, ComponentDetailView, CategoryDetailView
 
 urlpatterns = [
     path('', ComponentListView.as_view(), name='component-list'),
     path('component/<int:pk>/', ComponentDetailView.as_view(), name='component-detail'),
+    path('category/<int:pk>/', CategoryDetailView.as_view(), name='category-detail'),  # Add this line
 ]
 
--- website/website/catalog/views.py
+++ website/website/catalog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 from django.http import HttpResponse
 from django.db.models import Q
 from django.views.generic import ListView, DetailView
-from .models import Component
+from .models import Component, Category
 
 class ComponentDetailView(DetailView):
     model = Component
@@ -27,3 +28,17 @@ class ComponentListView(ListView):
                 Q(category__name__icontains=query)
             )
         return Component.objects.all()
+
+    class CategoryDetailView(ListView):
+        model = Component
+        template_name = 'catalog/category_detail.html'
+        context_object_name = 'components'
+
+        def get_queryset(self):
+            self.category = get_object_or_404(Category, pk=self.kwargs['pk'])
+            return Component.objects.filter(category=self.category)
+
+        def get_context_data(self, **kwargs):
+            context = super().get_context_data(**kwargs)
+            context['category'] = self.category
+            return context
