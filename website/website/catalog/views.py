6f968ae8d3e23c618d7197157b4cfd318e597880
Sofia Hnatiuk <sofia.hnatiuk46@gmail.com>
2024-10-04 20:54:24 +0300

    search function

--- website/website/catalog/views.py
+++ website/website/catalog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.db.models import Q
 from django.views.generic import ListView, DetailView
 from .models import Component
 
@@ -14,4 +15,15 @@ class ComponentListView(ListView):
     model = Component
     template_name = 'catalog/index.html'
     context_object_name = 'components'
-    paginate_by = 10  # Number of components per page
+    paginate_by = 10
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        if query:
+            return Component.objects.filter(
+                Q(name__icontains=query) |
+                Q(manufacturer__icontains=query) |
+                Q(description__icontains=query) |
+                Q(category__name__icontains=query)
+            )
+        return Component.objects.all()
